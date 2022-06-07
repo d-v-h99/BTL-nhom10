67fec2e293b69c4b0e06cf99f270b59d8f50e7e8
HUANBA <huan200102@gmail.com>
2022-06-07 23:34:20 +0700

    thêm trang login và trang admin

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+
+
+// var correct_user = 'admin';
+// var correct_pass = '123456';
+
+// var userName = document.getElementById("username");
+// var passWord = document.getElementById("password");
+
+// var formLogin = document.getElementById("form-login");
+
+// if(formLogin.attachEvent) {
+//     formLogin.attachEvent('submit', onFormSubmit);
+// }
+// else{
+//     formLogin.addEventListener('submit',onFormSubmit);
+// }
+
+// function onFormSubmit(e){
+//     var username = correct_user.value;
+//     var password = correct_pass.value;
+
+//     if(username == correct_user && password == correct_pass){
+//         alert("Đăng nhập thành công");
+//     }
+//     else{
+//         alert("Tài khoản mật khẩu không chính xác")
+//     }
+// }
