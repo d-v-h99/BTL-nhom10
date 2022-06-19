812d09a6850745c224ef54cb2dbbe355c29ba7a0
HUANBA <huan200102@gmail.com>
2022-06-19 12:05:47 +0700

    fix trang login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,3 @@
-
-
-
-// const userName = document.querySelector("#username");
-// const passWord = document.querySelector("#password");
-// const btnLogin = document.querySelector('#btn-login');
-
-// btnLogin.addEventListener('click', function(){
-//     if(userName.value == 'admin' && passWord.value == '12345') {
-//         document.location = './admin.html';
-//     }
-//     else if(userName.value == '' && passWord.value == ''){
-//         alert('chua nhap')
-//     }
-//     else{
-//         alert('??? sai')
-//     }
-   
-    
-// });
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+  });
