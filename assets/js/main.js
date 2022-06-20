75de7d2ed95358eaaadac6a8a793d17896215d27
HUANBA <huan200102@gmail.com>
2022-06-20 10:49:05 +0700

    thêm đăng ki va dang nhap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,53 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-  });
+
+
+
+function SignUp (event){
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var passwordConfirm = document.getElementById('password-confirm').value;
+    var user = {
+      user_name: username,
+      pass_word: password,
+      e_mail: email,
+    
+    };
+    if(password == passwordConfirm){
+
+      localStorage.setItem(username, JSON.stringify(user));
+      alert('dang ki thanh cong');
+    }
+    else {
+      alert('mat khau khong trung');
+    }
+    
+    
+  };
+
+
+
+function Login(event){
+  event.preventDefault();
+  var username = document.getElementById('username-login').value;
+  var password = document.getElementById('password-login').value;
+  var user = localStorage.getItem(username);
+  var data = JSON.parse(user);
+  if(username == '' || password == ''){
+    alert('chưa nhập tài khoản mật khẩu');
+    return;
+  }
+  if(user == null){
+    alert("tài khoản không tồn tại");
+  }
+  else if(username == data.user_name && password == data.pass_word ){
+    window.location.href = 'admin.html';
+  }
+  else{
+    alert('sai tài khoản hoặc mật khẩu')
+  }
+
+ 
+  
+
+}
